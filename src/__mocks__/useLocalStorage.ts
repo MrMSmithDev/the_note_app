6abe3bbf6a00a6625e609dff4a79cf38a908c07b
Michael <MichaelSmith90Dev@gmail.com>
2025-03-04 15:02:11 +0000

    Add updateNoteToLocal mock and tests

--- src/__mocks__/useLocalStorage.ts
+++ src/__mocks__/useLocalStorage.ts
@@ -2,11 +2,12 @@ export const mockSaveNote = jest.fn();
 export const mockRemoveNote = jest.fn();
 export const mockRetrieveNotes = jest.fn().mockReturnValue([]);
 export const mockLoadYear = jest.fn();
+export const mockUpdateNote = jest.fn();
 
 export const useLocalStorage = jest.fn(() => ({
-
   loadYear: mockLoadYear,
   saveNoteToLocal: mockSaveNote,
   removeNoteFromLocal: mockRemoveNote,
   retrieveNotesFromLocal: mockRetrieveNotes,
+  updateNoteToLocal: mockUpdateNote,
 }));
--- src/components/Note/Note.test.tsx
+++ src/components/Note/Note.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Note from './Note';
 import {
   mockSaveNote,
   mockRemoveNote,
   mockRetrieveNotes,
+  mockUpdateNote,
 } from '@__mocks__/useLocalStorage';
 import formatDate from '@utils/formatDate';
 
@@ -62,5 +63,55 @@ describe('Note component', () => {
 
     expect(mockRemoveNote).toHaveBeenCalledWith('2025-02-1', '1');
     expect(screen.queryByText('Test Note')).not.toBeInTheDocument();
+  });
+
+  it('sets a note to edit on edit button click, filled with the previous note', () => {
+    mockRetrieveNotes.mockReturnValue([{ id: '1', data: 'Test Note' }]);
+
+    render(<Note date="2025-02-1" />);
+
+    const editBtn = screen.getByRole('button', { name: 'Edit Note' });
+    fireEvent.click(editBtn);
+
+    expect(screen.getByTestId('note-to-update')).toHaveValue('Test Note');
+    expect(screen.getByRole('button', { name: 'Update' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+  });
+
+  it('updates a note with a new value when submitted', () => {
+    mockRetrieveNotes.mockReturnValue([{ id: '1', data: 'Test Note' }]);
+
+    render(<Note date="2025-02-1" />);
+
+    const editBtn = screen.getByRole('button', { name: 'Edit Note' });
+    fireEvent.click(editBtn);
+
+    const editInput = screen.getByTestId('note-to-update');
+    fireEvent.change(editInput, { target: { value: 'Test Note updated' } });
+
+    const submitBtn = screen.getByRole('button', { name: 'Update' });
+    fireEvent.click(submitBtn);
+
+    expect(screen.queryByTestId('note-to-update')).not.toBeInTheDocument();
+    expect(mockUpdateNote).toHaveBeenCalledWith("2025-02-1", { id: '1', data: 'Test Note updated' });
+    expect(screen.getByText('Test Note updated')).toBeInTheDocument();
+  });
+
+  it('cancels an update to a note when Cancel button clicked', () => {
+    mockRetrieveNotes.mockReturnValue([{ id: '1', data: 'Test Note' }]);
+
+    render(<Note date="2025-02-1" />);
+
+    const editBtn = screen.getByRole('button', { name: 'Edit Note' });
+    fireEvent.click(editBtn);
+
+    const editInput = screen.getByTestId('note-to-update');
+    fireEvent.change(editInput, { target: { value: 'Test Note updated' } });
+
+    const cancelBtn = screen.getByRole('button', { name: 'Cancel' });
+    fireEvent.click(cancelBtn);
+
+    expect(screen.queryByTestId('note-to-update')).not.toBeInTheDocument();
+    expect(screen.queryByText('Test Note updated')).not.toBeInTheDocument();
   });
 });
